8eee3a86f4bae85e97f824d909e897dd7d71b84d
Daniil <black3phoenix@gmail.com>
2020-07-26 00:32:39 +0300

    Updates

---
+++ precache-manifest.140779699f91b6ce2c3019e75f5ee7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f680549f49fcdcf6fb0bb060da55c145",
+    "url": "/react-covid19-map/index.html"
+  },
+  {
+    "revision": "b00e5d01806a9bec8089",
+    "url": "/react-covid19-map/static/css/main.3439bb77.chunk.css"
+  },
+  {
+    "revision": "65c02b1b9ac2e203f17d",
+    "url": "/react-covid19-map/static/js/2.20e15ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-covid19-map/static/js/2.20e15ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00e5d01806a9bec8089",
+    "url": "/react-covid19-map/static/js/main.c0c5e5ff.chunk.js"
+  },
+  {
+    "revision": "fe4255b7200952919ae1",
+    "url": "/react-covid19-map/static/js/runtime-main.54546cd4.js"
+  }
+]);
